c537166d907e048943345cc3944bfd5ca1c8d03b
Ramazan Aslan <91965312+enfyna@users.noreply.github.com>
2023-06-09 12:02:05 +0300

    Update Satranc.py

--- Satranc.py
+++ Satranc.py
@@ -2,33 +2,41 @@ from time import sleep
 import pygame
 import Tahta
 
+## 1- Tas Sec fonksiyonu ekle ona gore gidilebilen yerleri ciz
+
+## 2- Ondan sonra cizilen kareleri kaydet eger oyucu cizilen kareye tiklarsa tasi oraya oyna
+
+## 3- Sıra ekle beyaz siyah arasında
+
+## 4- Şah çekme ve oyunun bitmesini ekle
+
 def tahtayiciz(display):
     # Arkaplanı siyah yap
     display.fill((0,0,0))
 
     # Beyaz kareleri ciz
-    for y in range(0,boardSize,squareSize): 
+    for y in range(0,boardSize,squareSize):
         start = 0 if y//squareSize % 2 == 0 else squareSize
         for x in range(start,boardSize,squareSize*2):
             pygame.draw.rect(display, (100 , 100 , 100), [x,y,squareSize,squareSize])
 
 def taslariciz():
-    for i in Tahta.tahtaGuncelle():
-        renk = 25*i.takim+100
+    for i in Tahta.tahtaGuncelle(tahta , taslar):
+        renk = 50*i.takim+100
 
-        pygame.draw.rect(display, 
-            (renk , renk, renk), 
+        pygame.draw.rect(display,
+            (renk , renk, renk),
             [i.posX*squareSize+10,i.posY*squareSize+10,squareSize-20,squareSize-20],
             border_radius=20
-        )        
+        )
         # create a text surface object,
         # on which text is drawn on it.
         text = font.render(i.isim, False, (0,0,0))
-        
+
         # create a rectangular object for the
         # text surface object
         textRect = text.get_rect()
-        
+
         # set the center of the rectangular object.
         textRect.center = (i.posX*squareSize+squareSize/2,i.posY*squareSize+squareSize/2)
 
@@ -37,9 +45,32 @@ def taslariciz():
         # at the center coordinate.
         display.blit(text, textRect)
 
-def ekranguncelle(display):
+def gidilebilenyerlericiz(x,y):
+    for i in Tahta.gidebilir(x,y,tahta):
+        renk = (255,0,0)
+        pygame.draw.rect(display,renk,
+            [i[0]*squareSize+30,i[1]*squareSize+30,squareSize-60,squareSize-60],
+            border_radius=20
+        )
+    pass
+def tehditedilenyerlericiz():
+    for j in taslar:
+        for i in j.gidebilir():
+            renk = max(50,255*j.takim)
+            pygame.draw.rect(display,(renk,renk,renk),
+                [i[0]*squareSize+30,i[1]*squareSize+30,squareSize-60,squareSize-60],
+                border_radius=20
+            )
+    pass
+
+def ekranguncelle(display,x = None ,y = None):
     tahtayiciz(display)
     taslariciz()
+
+    tehditedilenyerlericiz()
+    if x != None and y != None:
+        gidilebilenyerlericiz(x,y)
+
     # Flip the display
     pygame.display.flip()
 
@@ -53,26 +84,27 @@ display = pygame.display.set_mode([boardSize, boardSize])
 # Font ayarla
 font = pygame.font.Font('freesansbold.ttf', 32)
 
-# Pencere başlığı 
+# Pencere başlığı
 pygame.display.set_caption("Satranç")
 
 # Tahtayı baslat
-Tahta.oyunuBaslat()
+tahta , taslar = Tahta.oyunuBaslat()
 
 #Ekran çizdir
 ekranguncelle(display)
 
 running = True
-while  running: # (taslar.bt(4) != 0 and taslar.st(4) != 0) and
+while running: # (taslar.bt(4) != 0 and taslar.st(4) != 0) and
+    sleep(.05)
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: # Did the user click the window close button?
+        if event.type == pygame.QUIT:
+            # Pencerenin X butonuna basıldıysa devam etmeye gerek yok
             running = False
+            break
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_presses = pygame.mouse.get_pressed()
             if mouse_presses[0]:
-                x , y = pygame.mouse.get_pos()
-                print("Left Mouse key was clicked at " + str(x) + " ," + str(y)) 
-                ekranguncelle(display)
-    sleep(.1)
+                x , y = map(lambda v : v // squareSize , pygame.mouse.get_pos())
+                ekranguncelle(display,x,y)
 
 pygame.quit()
