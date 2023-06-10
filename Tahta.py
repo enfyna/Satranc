7853251e243e40c08a881645d232e862d8a3864c
Ramazan Aslan <91965312+enfyna@users.noreply.github.com>
2023-06-10 17:36:46 +0300

    Update Tahta.py

--- Tahta.py
+++ Tahta.py
@@ -7,10 +7,10 @@ def oyunuBaslat():
     # ---
     # 0 -> ise o karede bulunan taşı kaydediyor
     taslar = []
-    
+        
     for i in range(8):
         taslar.append(Tas.Piyon(i, 1, 1, tahta))
-        taslar.append(Tas.Piyon(i, 6, -1, tahta))
+        taslar.append(Tas.Piyon(i, 6, -1, tahta)) 
     for i in range(2):
         taslar.append(Tas.Kale(i*7, 0, 1, tahta))
         taslar.append(Tas.Kale(i*7, 7, -1, tahta))
@@ -26,18 +26,23 @@ def oyunuBaslat():
 
     taslar.append(Tas.Kral(4, 0, 1, tahta))
     taslar.append(Tas.Kralice(3, 0, 1, tahta))
-
+   
     return tahta , taslar
 
 def tahtaGuncelle(tahta , taslar):
     for i in range(len(tahta)): # tahtayi sifirla
-        for j in range(len(tahta)):
+        for j in range(len(tahta[i])):
             tahta[i][j][0] = None
             tahta[i][j][1] = 0
     
     for i in taslar: # Taslari diz
-        tahta[i.posY][i.posX][1] = i
-
+        if i.yasiyor: # Tas yasiyorsa tahtaya koy 
+            tahta[i.posY][i.posX][1] = i
+            continue
+        if isinstance(i, Tas.Kral):
+            return i.takim
+        taslar.remove(i) # Yasamiyorsa listeden cikar
+    
     for i in taslar: # Taslarin tehditlerini hesapla
         i.tehdit()
 
@@ -45,10 +50,16 @@ def tahtaGuncelle(tahta , taslar):
 
 def yazdir(tahta , taslar):
     print("\n")
-    for i in range(7,-1,-1):
+    for i in range(8):#7,-1,-1):
         print(i+1,end=" - ")    
         for j in range(8):
-            print(tahta[i][j][1],end=" ")
+            if tahta[i][j][0] == None:
+                print(" N",end=" ")
+                continue
+            elif tahta[i][j][0] > -1:
+                print(" "+str(tahta[i][j][0]),end=" ")
+                continue
+            print(tahta[i][j][0],end=" ")
         print("")
     print("    ",end="")
     for i in range(8):
@@ -60,11 +71,16 @@ def yazdir(tahta , taslar):
 
 def gidebilir(x,y,tahta):
     if isinstance(tahta[y][x][1],Tas.Tas):
-        return tahta[y][x][1].gidebilir()
-    return []
+        return tahta[y][x][1].gidebilir() , tahta[y][x][1].takim
+    return [] , None
+
+def gotur(tk,x,y,tahta):
+    if isinstance(tahta[tk[1]][tk[0]][1],Tas.Tas):
+        tahta[tk[1]][tk[0]][1].hareket(x,y)
 
 if __name__ == "__main__":
     tahta , taslar = oyunuBaslat()
+
     tahtaGuncelle(tahta , taslar)
     yazdir(tahta , taslar)
     
